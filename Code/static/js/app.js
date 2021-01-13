744c483cee7d6b48bc26e3155e5fe2f6621d1cc8
Jingwen Sun <viviensun2009@gmail.com>
2021-01-13 17:32:40 -0500

    app push

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,4 +1,3 @@
-//global variable
 var data;
 
 //init function to fill in the select option
@@ -19,8 +18,8 @@ function init() {
         });
     });
   });
-}
-
+};
+  
 
 init();
 
@@ -48,5 +47,95 @@ function fixBacteria(name) {
     return listOuid;
   }
 
+//changing plots and demographic info based on selected data
+d3.selectAll("#selDataset").on("change", plotFunctions);
+
+function plotFunctions() {
+  var valueSelect = d3.select("#selDataset").node().value;
+  console.log(valueSelect);
+  demographicInfo(valueSelect);
+  barChart(valueSelect);
+  bubbleChart(valueSelect);
+  gaugeChart(valueSelect);
+};
+
+function demographicInfo(valueSelect) {
+    var filteredValue = data.metadata.filter(value => value.id == valueSelect);
+
+    var panel = d3.select(".panel-body");
+    panel.html("");
+    panel.append("p").text(`ID: ${filteredValue[0].id}`);
+    panel.append("p").text(`Ethnicity: ${filteredValue[0].ethnicity}`);
+    panel.append("p").text(`Gender: ${filteredValue[0].gender}`);
+    panel.append("p").text(`Age: ${filteredValue[0].age}`);
+    panel.append("p").text(`Location: ${filteredValue[0].location}`);
+    panel.append("p").text(`Bbtype: ${filteredValue[0].bbtype}`);
+    panel.append("p").text(`Wfreq: ${filteredValue[0].wfreq}`);
+    }
+
+function barChart(valueSelect) {
+  var filteredValue = data.samples.filter(value => value.id == valueSelect);
+  var ouid = filteredValue.map(d => d.otu_ids);
+  ouid = fixOuid(ouid[0].slice(0, 10));
+  var valueX = filteredValue.map(d => d.sample_values);
+  valueX = valueX[0].slice(0, 10);
+
+  var otu_label = filteredValue.map(d => d.otu_labels);
+  var names = fixBacteria(otu_label[0]).slice(0, 10);
+  console.log(ouid);
+  console.log(valueX);
+  console.log(otu_label);
+  console.log(names);
+
+  var trace = {
+    x: valueX,
+    y: ouid,
+    text: names,
+    type: "bar",
+    orientation: "h"
+  };
+
+  var layout = {
+    yaxis: {
+      autorange: "reversed"
+    }
+  };
+
+  var dataV = [trace];
+
+  Plotly.newPlot("bar", dataV, layout);
+}
+
+
+function bubbleChart(valueSelect) {
+  var filteredValue = data.samples.filter(value => value.id == valueSelect);
+  var ouid = filteredValue.map(d => d.otu_ids);
+  ouid = ouid[0];
+  var yValue = filteredValue.map(d => d.sample_values);
+  yValue = yValue[0];
+
+  var otu_label = filteredValue.map(d => d.otu_labels);
+  otu_label = fixBacteria(otu_label[0]);
+
+  var trace1 = {
+    x: ouid,
+    y: yValue,
+    mode: "markers",
+    marker: {
+      color: ouid,
+      size: yValue
+    },
+    text: otu_label
+  };
+
+  var data2 = [trace1];
+
+  var layout = {
+    showlegend: false,
+    xaxis: { title: "OTU ID" }
+  };
+
+  Plotly.newPlot("bubble", data2, layout);
+}
 
 
